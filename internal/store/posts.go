4694f6852e8cec0ed3239d2ecf5523320a9dc932
tienv1per <tienv1perds@gmail.com>
2025-02-20 21:50:34 +0700

    add post schema

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,44 @@ package store
 import (
 	"context"
 	"database/sql"
+	"github.com/lib/pq"
 )
 
 type PostsStore struct {
 	db *sql.DB
 }
 
-func (store *PostsStore) Create(context context.Context) error {
+type Post struct {
+	ID        int64    `json:"id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	UserID    int64    `json:"user_id"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+}
+
+func (store *PostsStore) Create(context context.Context, post *Post) error {
+	query := `
+		INSERT INTO posts (content, title, user_id, tags) 
+		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
+
+	err := store.db.QueryRowContext(
+		context,
+		query,
+		(*post).Content,
+		post.Title,
+		post.UserID,
+		pq.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
