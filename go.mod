09f03519007286c737d45bc811ce7daceb621f02
tienv1per <tienv1perds@gmail.com>
2025-02-20 21:50:16 +0700

    add user schema

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module postery
 
 go 1.23.5
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- internal/store/users.go
+++ internal/store/users.go
@@ -9,6 +9,33 @@ type UsersStore struct {
 	db *sql.DB
 }
 
-func (store *UsersStore) Create(ctx context.Context) error {
+type User struct {
+	ID        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	CreatedAt string `json:"created_at"`
+}
+
+func (store *UsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, created_at
+	`
+
+	err := store.db.QueryRowContext(
+		ctx,
+		query,
+		(*user).Username,
+		user.Email,
+		user.Password,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
