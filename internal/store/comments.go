611793ca2ac7d2c0418fd358684f15f1bfeb0875
tienv1per <tienv1perds@gmail.com>
2025-02-23 01:29:35 +0700

    create comment in post

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -50,5 +50,28 @@ func (store *CommentStore) GetByPostID(ctx context.Context, postID int64) ([]Com
 }
 
 func (store *CommentStore) Create(ctx context.Context, comment *Comment) error {
+	query := `
+		INSERT INTO comments (post_id, user_id, content)
+		VALUES ($1, $2, $3)
+		RETURNING id, created_at
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	err := store.db.QueryRowContext(
+		ctx,
+		query,
+		comment.PostID,
+		comment.UserID,
+		comment.Content,
+	).Scan(
+		&comment.ID,
+		&comment.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
