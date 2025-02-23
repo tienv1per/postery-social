c8c4fe4e598ded2647f663acabfe0b2cede55de6
tienv1per <tienv1perds@gmail.com>
2025-02-23 22:41:27 +0700

    filter feed

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -138,7 +138,10 @@ func (store *PostStore) GetUserFeed(ctx context.Context, userID int64, fq Pagina
 		LEFT JOIN comments c ON c.post_id = p.id
 		LEFT JOIN users u ON p.user_id = u.id
 		JOIN followers f ON f.follower_id = p.user_id 
-		WHERE f.user_id = $1
+		WHERE 
+		    f.user_id = $1 AND 
+		    (p.title ILIKE '%' || $4 || '%' OR p.content ILIKE '%' || $4 || '%') AND
+		    (p.tags @> $5 OR $5 = '{}')
 		OR p.user_id = $1
 		GROUP BY p.id, u.username
 		ORDER BY p.created_at ` + fq.Sort + ` LIMIT $2 OFFSET $3;
@@ -147,7 +150,7 @@ func (store *PostStore) GetUserFeed(ctx context.Context, userID int64, fq Pagina
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := store.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
+	rows, err := store.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
 	if err != nil {
 		return nil, err
 	}
