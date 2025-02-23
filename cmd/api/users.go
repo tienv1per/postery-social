3e3e575b05095c2ecb2273e063f3660a425c7012
tienv1per <tienv1perds@gmail.com>
2025-02-23 15:48:50 +0700

    add user context middleware

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -33,3 +33,42 @@ func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
 		app.internalServerError(w, r, err)
 	}
 }
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Middleware function to add a user object to the request context
+func (app *application) usersContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.internalServerError(w, r, err)
+			return
+		}
+
+		// Get the current request context
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetByID(ctx, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				app.notFoundResponse(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+
+		// Add "user" data to the context
+		ctx = context.WithValue(ctx, userCtx, user)
+		// Pass the updated request with new context to the next handler
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
